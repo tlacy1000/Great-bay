c17fcbc5a5ef29c4dfa8ddaa1edef725b3499da3
Lacy Tammy <38111420+tlacy1000@users.noreply.github.com>
2019-10-04 13:19:37 -0500

    Update greatbayseeds.sql

--- greatbayseeds.sql
+++ greatbayseeds.sql
@@ -1,18 +1,8 @@
-DROP DATABASE IF EXISTS great_bayDB;
 
-CREATE DATABASE great_bayDB;
-
-USE great_bayDB;
 
 INSERT INTO auction (item, category, currentBid)
 VALUES ("Elephant Tusk", "Black Market Tools", 9)
 
-CREATE TABLE auction (
-  item VARCHAR(50) NOT NULL,
-  category VARCHAR (50) NOT NULL,
-  currentBid INT NULL,
-  PRIMARY KEY (id)
-);
 
 
 --Create an option for users to post an item or bid on an item-->
